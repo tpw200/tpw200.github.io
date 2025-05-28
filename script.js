16683e7decd9cc6ea88bfc2f8ef519934ea91ce9
tpw200 <typwilt@gmail.com>
2025-05-28 14:15:04 -0500

    Changed

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-var time = new Date()
+var time = new Date();
 
-if (time.getHours() > 6 && time.getHours() <= 14) {
-     document.body.style.backgroundColor = "red"; 
+if (time.getHours() > 6 && time.getHours() <= 12) {
+     document.body.style.backgroundColor = "orange"; 
+} else if (time.getHours() > 12 && time.getHours() <= 18) {
+     document.body.style.backgroundColor = "yellow"; 
+} else if (time.getHours() > 18 && time.getHours() <= 21) {
+     document.body.style.backgroundColor = "blue"; 
+} else if (time.getHours() <= 6 || time.getHours() > 21) {
+     document.body.style.backgroundColor = "purple"; 
 }
